e4d8f40af9a8d65a0ddaac3dbd68ce12d851fbc9
KatyC25 <cundanok9@gmail.com>
2023-10-01 21:39:12 -0600

    Actualizadas dependencias

--- app/api/hello/route.ts
+++
@@ -1,8 +0,0 @@
-export async function GET(request: Request) {
-  return new Response("Hello, Next.js!");
-}
-
-export async function POST(request: Request) {
-  console.log(request.body);
-  return new Response("Test");
-}

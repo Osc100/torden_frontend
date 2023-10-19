08e3aec55efccd2097dd0d0d9afc42c99ff0de45
Osc100 <oscar.javier.marin.robleto@gmail.com>
2023-10-19 09:55:17 -0600

    Historial chats

--- app/dashboard/historial_chats/page.tsx
+++ app/dashboard/historial_chats/page.tsx
@@ -1,6 +1,28 @@
-import { date } from "zod";
+"use client";
+
+import { getRequest } from "@/utils/functions";
+import { useEffect, useState } from "react";
+
+interface Chat {
+	id: string;
+	created: string;
+	company_id: number;
+	// agents?: string[] | null; // Uncomment and import GPTMessage if needed
+	ai_description?: string | null;
+	client_name?: string | null;
+}
 
 export default function HistorialChatPage() {
+	const [chats, setChats] = useState<Chat[]>([]);
+
+	useEffect(() => {
+		getRequest("history").then((response) => {
+			response.json().then((data) => {
+				setChats(data.slice().reverse());
+			});
+		});
+	}, []);
+
 	return (
 		<div
 			className=" bg-gradient-to-b from-[#03577E] to-[#45ACAF] 
@@ -10,26 +32,26 @@ export default function HistorialChatPage() {
 				<h1 className="shadow-sm text-primario ">HISTORIAL DE CHATS</h1>
 			</div>
 			<div className="container px-10">
-				<table className=" bg-slate-50 rounded-xl overflow-hidden w-full    shadow-xl ">
+				<table className=" bg-slate-50 rounded-xl overflow-y-scroll w-full shadow-xl ">
 					<thead>
 						<tr className="text-xl h-10 text-primario  ">
-							<th className="border border-blue-400 ">AGENTE</th>
-							<th className="border border-blue-400">CLIENTE</th>
+							<th className="border border-blue-400 ">ID</th>
+							<th className="border border-blue-400">COMPAÑÍA</th>
 
 							<th className="border border-blue-400">FECHA</th>
 						</tr>
 					</thead>
 					<tbody className="overflow-y-scroll">
-						<FilaDatos
-							nombreAgente="Julio"
-							nombreCliente="Jairo Rodriguez"
-							fecha={new Date()}
-						/>
-						<FilaDatos
-							nombreAgente="Andy"
-							nombreCliente="Diana Martinez"
-							fecha={new Date()}
-						/>
+						{chats.map((chat, index) => (
+							<FilaDatos
+								key={`chat row ${index}`}
+								nombreAgente={chat.id}
+								nombreCliente={
+									chat.company_id === 1 ? "Torden" : chat.company_id.toString()
+								}
+								fecha={new Date(chat.created)}
+							/>
+						))}
 					</tbody>
 				</table>
 			</div>
@@ -46,7 +68,14 @@ interface DatosTabla {
 function FilaDatos(props: DatosTabla) {
 	return (
 		<tr className="text-xl text-black h-10">
-			<td className="border border-blue-400 pl-5 ">{props.nombreAgente}</td>
+			<td className="border border-blue-400 pl-5 ">
+				<a
+					href={`/dashboard/historial_chats/${props.nombreAgente}`}
+					className="text-blue-500 w-full"
+				>
+					{props.nombreAgente}
+				</a>
+			</td>
 			<td className="border border-blue-400 pl-5">{props.nombreCliente}</td>
 
 			<td className="border border-blue-400 pl-5">

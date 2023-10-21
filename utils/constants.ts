e7da0fb8cab956ba825fcb2068a3beee482119b4
Osc100 <oscar.javier.marin.robleto@gmail.com>
2023-10-21 09:13:47 -0600

    Deployment test outside localhost

--- utils/constants.ts
+++ utils/constants.ts
@@ -1,7 +1,8 @@
-export const isProduction = process.env.IS_PRODUCTION !== undefined;
+// export const isProduction = process.env.IS_PRODUCTION !== undefined;
+export const isProduction = true;
 
 export const PROTOCOL = isProduction ? "https" : "http";
 export const WS_PROTOCOL = isProduction ? "wss" : "ws";
 
-export const API_HOST = process.env.API_HOST ?? "localhost:4000";
+export const API_HOST = "api.torden.services" ?? "localhost:4000";
 export const API_URL = `${PROTOCOL}://${API_HOST}`;
